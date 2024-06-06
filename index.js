b3ed6cd2584948e179323cbf7234a25e7f46bd55
abimbolaalabi <139625252+abimbolaalabi@users.noreply.github.com>
2024-06-06 08:19:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+ // Welcome to 30daysofJavascript!
+
+
+
+ /*
+    comments can make code readable,
+    easy to reuse
+    and informative
+*/
---
+++ variable.js
@@ -0,0 +1,27 @@
+// let firstName = 'Ibrahim';   // Strings
+// let lastName = 'Alabi';   // Strings
+// let sureName = null;       // NULL
+// let userName;              // Undefined
+
+// let age =  27:;             // Number
+
+// let isMuslim = true;       // Boolean
+// let doesHatesToCode = false;
+
+// let sampleArray = [0,1,2,3,4,5]   // Array
+
+const person = {
+    firstName: 'Ibrahim',
+    lastName: 'Alabi',
+    userName: 'adeabimbola'
+}
+
+
+let firstName = 'Ibrahim' ,
+lastName = 'Alabi' ,
+sureName = null,
+userName,
+age = 27
+isMuslim = true
+doesHateToCode = false
+sampleArray = [0,1,2,3,4,5];
